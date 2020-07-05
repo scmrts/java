6b03d14828325a8d1528aa6862fb5dbda45a9cf3
scmrts <scmrts@naver.com>
2020-07-05 18:31:18 +0900

    3번까지

--- ssp/ssp_java_test-2018/SUB1/src/com/lgcns/test/RunManager.java
+++ ssp/ssp_java_test-2018/SUB1/src/com/lgcns/test/RunManager.java
@@ -1,10 +1,61 @@
 package com.lgcns.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.StringJoiner;
+
+import org.omg.CORBA.IntHolder;
+import org.omg.CORBA.StringHolder;
+
 public class RunManager {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		Files.walkFileTree(Paths.get("./BIGFILE"), new SimpleFileVisitor<Path>() {
+			@Override
+		    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Path path = Paths.get("./OUTPUT/"+file.getFileName().toString());
+				if(Files.notExists(Paths.get("./OUTPUT/"))) {
+					Files.createDirectory(Paths.get("./OUTPUT/"));
+				}
+				if(Files.exists(path)) {
+					Files.delete(path);
+				} 
+				
+				List<String> lines = Files.readAllLines(file);
+				String tmp = null;
+				
+				StringJoiner stj = new StringJoiner("\n");
+				for (int i = 0; i < lines.size(); i++) {
+					int count = 1;
+					String cu = lines.get(i);
+					for (int j = i + 1; j < lines.size(); j++) {
+						if(! cu.equals(lines.get(j))) {
+							break;
+						}
+						count++;
+						i++;
+					}
+					if(count > 1) {
+//						stj.add(count+"#"+cu);
+						
+						Files.write(path, (count+"#"+cu+"\n").getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+					} else {
+//						stj.add(cu);
+						Files.write(path, (cu+"\n").getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND, StandardOpenOption.WRITE);
+					}
+					
+				}
+				
+				return FileVisitResult.CONTINUE;
+			}
+		});
 	}
-
 }
--- ssp/ssp_java_test-2018/SUB2/src/com/lgcns/test/RunManager.java
+++ ssp/ssp_java_test-2018/SUB2/src/com/lgcns/test/RunManager.java
@@ -1,10 +1,91 @@
 package com.lgcns.test;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+
 public class RunManager {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		Files.walkFileTree(Paths.get("./BIGFILE"), new SimpleFileVisitor<Path>() {
+			@Override
+		    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Path path = Paths.get("./OUTPUT/"+file.getFileName().toString());
+				if(Files.notExists(Paths.get("./OUTPUT/"))) {
+					Files.createDirectory(Paths.get("./OUTPUT/"));
+				}
+				if(Files.exists(path)) {
+					Files.delete(path);
+				} 
+				
+				List<String> lines = Files.readAllLines(file);
+				StringJoiner stj = new StringJoiner("\n");
+				for (int i = 0; i < lines.size(); i++) {
+					int count = 1;
+					String cu = lines.get(i);
+					for (int j = i + 1; j < lines.size(); j++) {
+						if(! cu.equals(lines.get(j))) {
+							break;
+						}
+						count++;
+						i++;
+					}
+					if(count > 1) {
+						stj.add(count+"#"+cu);
+						
+					} else {
+						stj.add(cu);
+					}
+					
+				}
+				
+				String collect = Arrays.stream(stj.toString().split("\n")).map(line -> {
+					char[] chLine = line.toCharArray();
+					String ret = "";
+					for (int i = 0; i < chLine.length; i++) {
+						int count = 1;
+						char cu = chLine[i];
+						for (int j = i + 1; j < chLine.length; j++) {
+							if(cu != chLine[j]) {
+								break;
+							}
+							count++;
+							i++;
+						}
+						if(count > 2) {
+							ret += count+Character.toString(cu);
+							
+						} else if (count == 1){
+							ret += Character.toString(cu);
+						} else {
+							BiFunction<Integer, Character, String> func = (a, b) -> {
+								String out = "";
+								for(int k = 0 ;k < a ; k++) {
+									out += Character.toString(b);
+								}
+								return out;
+							};
+							ret += func.apply(count, cu);
+						}
+					}
+					return ret;
+				}).collect(Collectors.joining("\n"));
+				
+				Files.write(path, collect.getBytes());
+				
+				return FileVisitResult.CONTINUE;
+			}
+		});
 	}
-
 }
